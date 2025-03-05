93b703d4e86e965c3c0aa2502ed29edd3e3a92f1
Pritesh <bhuravanepritesh@gmail.com>
2025-03-05 07:25:19 +0530

    Added multiply and divide functions

--- utils.js
+++ utils.js
@@ -37,5 +37,6 @@ function buggyFunction() {
 module.exports = { add, subtract, multiply, divide, isEven, isOdd, buggyFunction };
 
 function multiply(a, b) { return a * b; }
+
+function multiply(a, b) { return a * b; }
 function divide(a, b) { return a / b; }
-function buggyFunction() { return undefinedVar + 10; }
